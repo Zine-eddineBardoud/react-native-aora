c569c1403d446623e87ac3eaec041e49873b9e62
Zine-eddineBardoud <zineddinebardoud38@gmail.com>
2024-08-14 11:26:50 +0100

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,55 @@
+import { View, Text, LogBox, ScrollView, Image } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Link, router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
+
+export default function HomeScreen() {
+    return (
+        <SafeAreaView className="bg-primary h-full">
+            <ScrollView contentContainerStyle={{ height: "100%" }}>
+                <View className="w-full flex justify-center items-center h-full min-h-[85vh] px-8">
+                    <Image
+                        source={images.logo}
+                        className="w-[130px] h-[84px]"
+                        resizeMode='contain'
+                    />
+
+                    <Image
+                        source={images.cards}
+                        className="max-w-[380px] w-full h-[300px]"
+                        resizeMode='contain'
+                    />
+
+                    <View className='relative mt-5'>
+                        <Text className='text-3xl text-white font-bold text-center'>
+                            Discover Endless Possibilities with {' '}
+                            <Text className='text-secondary-200'>Aora</Text>
+                        </Text>
+                        <Image
+                            source={images.path}
+                            className='w-[136px] h-[15px] absolute -bottom-2 -right-8'
+                            resizeMode='contain'
+                        />
+                    </View>
+
+                    <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>Where creativity meets innovatio: embark on a journey of limitless exploration with Aora</Text>
+
+                    <CustomButton
+                        title='Continue with Email'
+                        handlePress={() => router.push('/sign-in')}
+                        containerStyles='w-full mt-7'
+                    />
+                </View>
+            </ScrollView>
+
+            <StatusBar backgroundColor='#161622' style='light' />
+        </SafeAreaView>
+    );
+}
+
+LogBox.ignoreLogs([
+    '[Reanimated] Reduced motion setting is enabled on this device.',
+]);
